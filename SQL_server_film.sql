77855bfa3092c87adac34dca10ad52d03b93bff9
d3ede <theskonel@gmail.com>
2025-01-30 11:40:07 +0200

    SQL 4

--- SQL_server_film.sql
+++ SQL_server_film.sql
@@ -28,3 +28,33 @@ FOREIGN KEY (zanrID) REFERENCES zanr(zanrID);
 SELECT * FROM film;
 SELECT * FROM zanr;
 UPDATE film SET zanrID=2 WHERE filmID=2
+
+CREATE TABLE rezisoor(
+rezID int PRIMARY KEY identity (1,1),
+rezNimi varchar(25) not null,
+rezSunniaeg int,
+rezRiik varchar(30));
+
+SELECT * FROM rezisoor;
+
+INSERT INTO rezisoor(rezNimi, rezSunniaeg, rezRiik)
+VALUES('Nolan', 1976, 'USA'), ('RidleyScott', 1842, 'Cambogua')
+
+ALTER TABLE film ADD rezID int;
+ALTER TABLE film ADD CONSTRAINT fk_rez
+FOREIGN KEY (rezID) REFERENCES rezisoor(rezID);
+UPDATE film SET rezID=1 WHERE filmID=2
+
+CREATE TABLE kinokava(
+kinokavID int PRIMARY KEY identity (1,1),
+kinokavTime int);
+
+ALTER TABLE kinokava ADD filmID int;
+ALTER TABLE kinokava ADD CONSTRAINT fk_film
+FOREIGN KEY (filmID) REFERENCES film(filmID);
+INSERT INTO kinokava(kinokavTime)
+VALUES(2), (3), (1), (2)
+UPDATE kinokava SET filmID=3 WHERE kinokavID=3
+
+SELECT * FROM kinokava;
+SELECT * FROM film;
